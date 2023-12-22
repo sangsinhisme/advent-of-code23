97b0a35e113aa7ee0e84f0bf3cea49c50c09aae2
Sinh Nguyen sang <@sinhns2>
2023-12-22 13:38:01 +0700

    add: day8 part2

--- src/main/scala/wasteland/Main.scala
+++ src/main/scala/wasteland/Main.scala
@@ -1,11 +1,19 @@
 package wasteland
 
+import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.matching.Regex
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  @tailrec
+  def gcd(a: Long, b: Long): Long = if (b==0) a else gcd(b, a % b)
+
+  def lcm(a: Long, b: Long): Long = {
+    (a * b).abs / gcd(a, b)
+  }
+
+  def _main(args: Array[String]): Unit = {
     val source = Source.fromFile("src/main/scala/wasteland/puzzle_day8.txt")
     val lines =
       try source.getLines().toList
@@ -22,37 +30,34 @@ object Main {
       }
     }
 
-    var visitNode = document.filter {
+    val visitNode = document.filter {
       x => x._1.last == 'A'
     }.keys.toArray
 
-    var step = 0
-    var loop = 0
-    while (visitNode.count(x => x.last == 'Z') != visitNode.length) {
-      if (step > instruction.length) {
-        val navigate = instruction.apply(step)
-        step += 1
+    var result: Long = 1
+    for (node <- visitNode) {
+      var step: Long = 0
+      var newNode = node
+      var shouldBreak = false
+      while (!newNode.endsWith("Z") && !shouldBreak) {
+        for (navigate <- instruction) {
+          newNode = document.get(newNode) match
+            case Some((left, right)) =>
+              navigate match
+                case 'L' => left
+                case 'R' => right
+            case _ => newNode
+          if newNode.endsWith("Z") then {
+            step += 1
+            shouldBreak = true
+          }
+          if !shouldBreak then step +=1
+        }
       }
-      else {
-        step = 0
-        loop += 1
-      }
-    }
+      result = lcm(step, result)
+      println(s"$node $newNode $step $result")
 
-//    var visited: Array[String] = Array("AAA")
-//    while (visited.last != "ZZZ") {
-//      for (navigate <- instruction) {
-//        document.get(visited.last) match {
-//          case Some((left, right)) =>
-//            navigate match
-//              case 'L' => visited = visited :+ left
-//              case 'R' => visited = visited :+ right
-//
-//          case None =>
-//        }
-//      }
-//    }
-//
-//    println(visited.length - 1)
+    }
+    println(result)
   }
 }
