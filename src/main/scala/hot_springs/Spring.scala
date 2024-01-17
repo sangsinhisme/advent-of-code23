3b50ffa288988d09d94e8d962275638924c42ebe
sinhns2 <sinhns2@fpt.com>
2024-01-17 13:37:17 +0700

    day12: part1 passed

--- src/main/scala/hot_springs/Spring.scala
+++ src/main/scala/hot_springs/Spring.scala
@@ -2,9 +2,21 @@ package hot_springs
 import scala.util.matching.Regex
 
 class Spring {
-  def is_criteria(engineers: String, condition: Array[Int]): Boolean = {
+  def is_criteria(eng: String, cond: Array[Int]): Boolean = {
     val regex_agg: Regex = "#+".r
-    val map_input = regex_agg.findAllMatchIn(engineers).toArray.map(x => x.matched.length)
-    map_input sameElements condition
+    val map_input = regex_agg.findAllMatchIn(eng).toArray.map(x => x.matched.length)
+    map_input sameElements cond
+  }
+
+  def f(eng: String, cond: Array[Int], i: Int): Int = {
+    if (i == eng.length) {
+      if is_criteria(eng, cond) then 1 else 0
+    }
+    else {
+      if eng(i) == '?' then
+        f(eng.updated(i, '#'), cond, i + 1) +
+        f(eng.updated(i, '.'), cond, i + 1)
+      else f(eng, cond, i + 1)
+    }
   }
 }
