b6ae1e0a68796596814cc2de9a7c5bb4e45737be
sinhns2 <sinhns2@fpt.com>
2024-01-25 12:36:20 +0700

    day13: done part2

--- src/main/scala/point_incidence/Mirror.scala
+++ src/main/scala/point_incidence/Mirror.scala
@@ -10,40 +10,53 @@ class Mirror {
       input_half: Array[String],
       output_half: Array[String]
   ): Boolean = {
-    if (
-      input_half.length != output_half.length
-    ) {
+    if (input_half.length != output_half.length) {
       false
     } else {
       input_half.indices.forall { i =>
         input_half(i) == output_half(output_half.length - 1 - i)
       }
+      if (fix_smudge(input_half, output_half)) true
+      else false
     }
   }
 
-  def mirror_horizon(input: Array[String]): Int = {
+  def mirror_horizon(input: Array[String], i: Int = 1): Int = {
     val length = input.length
     val mid = length / 2
-    var mirror = 0
-    for (i <- 1 until input.length) {
-      if (i <= mid) {
-        if is_reflection(input.take(i), input.slice(i, i + i)) then {
-          mirror = i
-        }
-      } else {
-        if is_reflection(input.slice(i - input.drop(i).length, i), input.drop(i)) then {
-          mirror = i
-        }
-      }
+    if (i < input.length) {
+      val isReflection =
+        if (i <= mid) is_reflection(input.take(i), input.slice(i, i + i))
+        else
+          is_reflection(input.slice(i - input.drop(i).length, i), input.drop(i))
+      if (isReflection) i
+      else mirror_horizon(input, i + 1)
+    } else {
+      0
     }
-    mirror
+  }
+
+  def fix_smudge(
+      input_half: Array[String],
+      output_half: Array[String]
+  ): Boolean = {
+    val reflect = input_half.indices.map { i =>
+      input_half(i).zip(output_half(output_half.length - 1 - i)).count {
+        case (c1, c2) => c1 == c2
+      }
+    }.sum
+
+    if (reflect == (input_half.length * input_half.head.length - 1)) {
+      true
+    } else false
   }
 
   def sumOfMirrorIndex(input: Array[String]): Int = {
     val horizon = mirror_horizon(input)
-    val result = if horizon != 0 then horizon * 100
-    else mirror_horizon(input.map(_.toList).toList.transpose.map(_.mkString).toArray)
-    result
+    val vertical = mirror_horizon(
+      input.map(_.toList).toList.transpose.map(_.mkString).toArray
+    )
+    horizon * 100 + vertical
   }
 
   def calling_sum(
--- src/test/scala/point_incidence/MirrorTest.scala
+++ src/test/scala/point_incidence/MirrorTest.scala
@@ -4,14 +4,23 @@ class MirrorTest extends munit.FunSuite:
   import point_incidence.Mirror
   import utils.CommonUtils
 
-  test ("is valid mirror?") {
+  test ("is valid mirror ?") {
     assert(1 == 1)
   }
 
-  test ("is same pattern") {
+  test ("is same pattern ?") {
     val source =
       CommonUtils.convert2string("src/test/scala/point_incidence/01.txt")
     val test =
         CommonUtils.convert2string("src/test/scala/point_incidence/02.txt")
     assert(Mirror().is_reflection(source, test))
+  }
+
+
+  test ("fix smudge result?") {
+    val source =
+      CommonUtils.convert2string("src/test/scala/point_incidence/01.txt")
+    val result: Long = Mirror().calling_sum(source, Array.empty)
+    println(s"My result: $result")
+    assert(result == 7)
   }
