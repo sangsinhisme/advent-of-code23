1f59596df1cfe75806c5a7b497bf1c2c71ec444b
sinhns2 <sinhns2@fpt.com>
2024-01-30 16:14:49 +0700

    day15: part2

--- src/test/scala/lens_library/HashTest.scala
+++ src/test/scala/lens_library/HashTest.scala
@@ -9,7 +9,6 @@ class HashTest extends munit.FunSuite:
     val true_ground = List(72, 65, 83, 72, 45)
     val test = input.toList.map(char => char.toByte)
     assert(test == true_ground)
-
     val input_hash = "rn=1"
     val decode = 30
     assert(Hash().hash_to_ascii(input_hash) == decode)
@@ -24,4 +23,28 @@ class HashTest extends munit.FunSuite:
       Hash().hash_to_ascii(word)
     }).sum
     assert(ans == 1320)
+  }
+
+  test("focusing power ?") {
+    val hash = new Hash()
+    var boxes = List(
+      hash.Box(0, List.empty)
+    )
+    val source = CommonUtils.convert2list("src/test/scala/lens_library/01.txt")
+    val sequence = source.mkString.split(",")
+    for (word <- sequence){
+      boxes = hash.lens_hashmap(word, boxes)
+    }
+    val result = (for {
+      box <- boxes
+    } yield {
+      val index = box.index
+      val lens = box.lens
+      (for {
+        len <- lens
+      } yield {
+        (index + 1) * (lens.indexWhere(x => x == len) + 1) * len.length
+      }).sum
+    }).sum
+    assert(result == 145)
   }
